53d11f3416b809f7d9a828ae18e72c1f6236b56f
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2024-04-03 19:11:56 -0700

    Remove user registration routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('news', NewsArticleController::class);
+    Route::resource('/news', NewsArticleController::class);
 });
 
 require __DIR__ . '/auth.php';
