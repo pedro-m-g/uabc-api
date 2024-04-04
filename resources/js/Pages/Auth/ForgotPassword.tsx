13be2865c2915b87b4b4045e3a88ca80059d829d
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2024-04-03 17:25:52 -0700

    Translate auth components

--- resources/js/Pages/Auth/ForgotPassword.tsx
+++ resources/js/Pages/Auth/ForgotPassword.tsx
@@ -18,11 +18,10 @@ export default function ForgotPassword({ status }: { status?: string }) {
 
     return (
         <GuestLayout>
-            <Head title="Forgot Password" />
+            <Head title="Olvidé mi contraseña" />
 
             <div className="mb-4 text-sm text-gray-600">
-                Forgot your password? No problem. Just let us know your email address and we will email you a password
-                reset link that will allow you to choose a new one.
+                ¿Olvidaste tu contraseña? No te preocupes. Escribe aquí tu correo electrónico y te enviaremos un enlace para recuperar tu cuenta.
             </div>
 
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
@@ -42,7 +41,7 @@ export default function ForgotPassword({ status }: { status?: string }) {
 
                 <div className="flex items-center justify-end mt-4">
                     <PrimaryButton className="ms-4" disabled={processing}>
-                        Email Password Reset Link
+                        Enviar enlace de recuperación de cuenta
                     </PrimaryButton>
                 </div>
             </form>
