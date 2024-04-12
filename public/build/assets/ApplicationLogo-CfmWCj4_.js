9350b444661c23e37a7efc2b039a2455b220100c
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2024-04-12 04:47:46 -0700

    Add frontend assets to repo

---
+++ public/build/assets/ApplicationLogo-CfmWCj4_.js
@@ -0,0 +1,1 @@
+import{j as i}from"./app-DllE_dMi.js";function t(o){return i.jsx("img",{src:"/images/logo-uabc.png",alt:"Escudo Oficial UABC",...o})}export{t as A};
---
+++ public/build/assets/Table-CR4MIAPA.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-DllE_dMi.js";function r({header:s,children:t,...a}){return e.jsxs("table",{className:"table-auto w-full border-collapse my-4",...a,children:[e.jsx("thead",{children:s}),e.jsx("tbody",{children:t})]})}export{r as T};
