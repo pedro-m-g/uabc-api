9f874b4b1d0f764a845d1384e75dc131483ad00e
Pedro Martin del Campo Gonzalez <pedro.martindelcampo.gonzalez@gmail.com>
2024-04-12 06:29:18 -0700

    Update assets

--- public/assets/ApplicationLogo-ghsLxUaj.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-orEJ4tBh.js";const t="/public/assets/logo-uabc-Dac7vztO.png";function i(o){return s.jsx("img",{src:t,alt:"Escudo Oficial UABC",...o})}export{i as A};
--- public/assets/Table-UUjvHKI_.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-orEJ4tBh.js";function r({header:s,children:t,...a}){return e.jsxs("table",{className:"table-auto w-full border-collapse my-4",...a,children:[e.jsx("thead",{children:s}),e.jsx("tbody",{children:t})]})}export{r as T};
